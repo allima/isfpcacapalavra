f4f28c84d5bf307afd81813e9d3a1f1a81ce552a
Alexandre de Lima <allima1991@gmail.com>
2015-02-11 15:52:34 -0200

    inicio

---
+++ cacapalavra/src/test/java/br/com/ifsp/cacapalavra/modelo/PastaTest.java
@@ -0,0 +1,14 @@
+package br.com.ifsp.cacapalavra.modelo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PastaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
