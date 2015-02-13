6b722e4a4d399db5a725e40e18339c6df7ed3080
Alexandre de Lima <allima1991@gmail.com>
2015-02-13 16:48:12 -0200

    test novas classes

---
+++ cacapalavra/src/main/java/br/com/ifsp/cacapalavra/modelo/ExtensaoTest.java
@@ -0,0 +1,14 @@
+package br.com.ifsp.cacapalavra.modelo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExtensaoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
